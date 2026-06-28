4d8102ed771b40247d02b145abb884afe6be9cef
PutraOFC <115691638+VinnKanaichi@users.noreply.github.com>
2026-06-28 16:35:32 +0700

    Update telegram.js

--- api/telegram.js
+++ api/telegram.js
@@ -136,7 +136,7 @@ export default async function handler(req, res) {
 
   res.setHeader(
     "Set-Cookie",
-    "session=; HttpOnly; Secure; SameSite=Strict; Path=/; Max-Age=0"
+    "session=; HttpOnly; Secure; SameSite=Lax; Path=/; Max-Age=0"
   );
 
   return res.status(200).json({
